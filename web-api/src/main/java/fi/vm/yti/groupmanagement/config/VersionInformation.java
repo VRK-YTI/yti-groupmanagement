629cdecd7153aa85e8b3ac724d99d2bcb79de97d
Kalle Kyttälä <kalle.kyttala@rebase.fi>
2021-09-06 14:01:26 +0300

    add version info route

---
+++ web-api/src/main/java/fi/vm/yti/groupmanagement/config/VersionInformation.java
@@ -0,0 +1,20 @@
+package fi.vm.yti.groupmanagement.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.stereotype.Component;
+
+@Component
+@PropertySource(value = "classpath:git.properties", ignoreResourceNotFound = true)
+public class VersionInformation {
+
+    @Value(value = "${git.commit.id.abbrev:dev}")
+    private String commitId;
+
+    @Value(value = "${git.branch}")
+    private String branch;
+
+    public String getVersion() {
+        return "build " + commitId + ", branch " + branch;
+    }
+}
